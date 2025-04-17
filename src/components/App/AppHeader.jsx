a5cd3be5b523c4bfa2341b31e4f1f0289a2a15e4
Stefan Pfahler <stefan.pfahler@evosoft.com>
2025-04-17 10:21:12 +0200

    schere

--- src/components/App/AppHeader.jsx
+++ src/components/App/AppHeader.jsx
@@ -240,7 +240,7 @@ export default function AppHeader() {
 
           <AppHeaderButton onClick={openShareDialog}>
             <Share />
-            <div className="text-xs">Share</div>
+            <div className="text-xs">Schere</div>
           </AppHeaderButton>
 
           <div className="hidden md:block">
